de4f6ddfeb46e254422413be8f1734c8609b2b18
Egor Sabyanin <egor.sabyanin.dev@gmail.com>
2023-06-14 23:22:16 +0700

    feat: настройки cors

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
+
+// CORS Settings
+
+const allowedCors = [
+  'http://mesto-frontend.nomoredomains.rocks',
+  'https://mesto-frontend.nomoredomains.rocks',
+  'localhost:3000',
+  'http://localhost',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin: allowedCors,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+// Middlewares
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const handleUnexpectedErrorsMiddleware = require('./middlewares/handleUnexpectedErrors');
 
 const { PORT = 3000, DB_CONNECTION = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -9,10 +31,14 @@ const app = express();
 const router = require('./routes/index');
 
 app.use(express.json());
-
+app.use(requestLogger);
+app.use(cors(corsOptions));
 app.use(router);
 
+// Обработка ошибок
+app.use(errorLogger);
 app.use(errors());
+app.use(handleUnexpectedErrorsMiddleware);
 
 // Подключение к БД
 mongoose.connect(DB_CONNECTION, {
